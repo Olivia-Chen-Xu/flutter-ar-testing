b7a3d0582056cbcbba0a8131321b78d33c1305e9
Olivia-Chen-Xu <oliviachenxu@gmail.com>
2021-10-16 08:37:30 -0400

    alternative approach

--- lib/augmented_images.dart
+++ lib/augmented_images.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import "package:vector_math/vector_math_64.dart" as vector;
 
 class AugementedImages extends StatefulWidget {
   const AugementedImages({Key? key}) : super(key: key);
@@ -11,53 +12,80 @@ class AugementedImages extends StatefulWidget {
 
 class _AugementedImagesState extends State<AugementedImages> {
   late ArCoreController arCoreController;
-  Map<int, ArCoreAugmentedImage> augmentedImagesMap = {};
-  whenArCoreViewCreated(ArCoreController coreController) {
-    arCoreController = coreController;
-    arCoreController.onTrackingImage = controlOnTrackingImage;
 
-    //load the single image
+  // Map<int, ArCoreAugmentedImage> augmentedImagesMap = {};
+  // whenArCoreViewCreated(ArCoreController coreController) {
+  //   arCoreController = coreController;
+  //   arCoreController.onTrackingImage = controlOnTrackingImage;
 
-    loadSingleImage();
+  //   //load the single image
+
+  //   loadSingleImage();
+  // }
+
+  // loadSingleImage() async {
+  //   final ByteData bytes =
+  //       await rootBundle.load('assets/earth_augmented_image.jpg');
+  //   arCoreController.loadAugmentedImagesDatabase(
+  //     bytes: bytes.buffer.asUint8List(),
+  //   );
+  // }
+
+  // controlOnTrackingImage(ArCoreAugmentedImage augmentedImage) {
+  //   if (!augmentedImagesMap.containsKey(augmentedImage.index)) {
+  //     augmentedImagesMap[augmentedImage.index] = augmentedImage;
+  //     //add sphere
+  //     addSphere(augmentedImage);
+  //   }
+  // }
+
+  // addSphere(ArCoreAugmentedImage arCoreAugmentedImage) async {
+  //   final ByteData textureBytes = await rootBundle.load('assets/earth.jpg');
+  //   final material = ArCoreMaterial(
+  //     color: const Color.fromARGB(120, 66, 134, 244),
+  //     textureBytes: textureBytes.buffer.asUint8List(),
+  //   );
+  //   final sphere = ArCoreSphere(
+  //     materials: [material],
+  //     radius: arCoreAugmentedImage.extentX / 2,
+  //   );
+  //   final node = ArCoreNode(
+  //     shape: sphere,
+  //   );
+  //   arCoreController.addArCoreNodeToAugmentedImage(
+  //       node, arCoreAugmentedImage.index);
+  // }
+
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  //   arCoreController.dispose();
+  // }
+
+  void whenArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+    arCoreController.onPlaneTap = controlOnPlaneTap;
   }
 
-  loadSingleImage() async {
-    final ByteData bytes =
-        await rootBundle.load('assets/earth_augmented_image.jpg');
-    arCoreController.loadAugmentedImagesDatabase(
-      bytes: bytes.buffer.asUint8List(),
-    );
+  void controlOnPlaneTap(List<ArCoreHitTestResult> results) {
+    final hit = results.first;
+
+    addImage(hit);
   }
 
-  controlOnTrackingImage(ArCoreAugmentedImage augmentedImage) {
-    if (!augmentedImagesMap.containsKey(augmentedImage.index)) {
-      augmentedImagesMap[augmentedImage.index] = augmentedImage;
-      //add sphere
-      addSphere(augmentedImage);
-    }
-  }
+  addImage(ArCoreHitTestResult hitTestResult) async {
+    final bytes = (await rootBundle.load('assets/earth_augmented_image.jpg'))
+        .buffer
+        .asUint8List();
 
-  addSphere(ArCoreAugmentedImage arCoreAugmentedImage) async {
-    final ByteData textureBytes = await rootBundle.load('assets/earth.jpg');
-    final material = ArCoreMaterial(
-      color: const Color.fromARGB(120, 66, 134, 244),
-      textureBytes: textureBytes.buffer.asUint8List(),
+    final imageNew = ArCoreNode(
+      image: ArCoreImage(bytes: bytes, width: 60),
+      position: hitTestResult.pose.translation + vector.Vector3(0.0, 0.0, 0.0),
+      rotation:
+          hitTestResult.pose.rotation + vector.Vector4(0.0, 0.0, 0.0, 0.0),
     );
-    final sphere = ArCoreSphere(
-      materials: [material],
-      radius: arCoreAugmentedImage.extentX / 2,
-    );
-    final node = ArCoreNode(
-      shape: sphere,
-    );
-    arCoreController.addArCoreNodeToAugmentedImage(
-        node, arCoreAugmentedImage.index);
-  }
 
-  @override
-  void dispose() {
-    super.dispose();
-    arCoreController.dispose();
+    arCoreController.addArCoreNodeWithAnchor(imageNew);
   }
 
   @override
@@ -69,7 +97,7 @@ class _AugementedImagesState extends State<AugementedImages> {
         ),
         body: ArCoreView(
           onArCoreViewCreated: whenArCoreViewCreated,
-          type: ArCoreViewType.AUGMENTEDIMAGES,
+          enableTapRecognizer: true,
         ));
   }
 }
