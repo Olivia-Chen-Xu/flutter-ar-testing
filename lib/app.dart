62af9aefb4ebd9be18d48dbfdcfb08d8bf764c29
Olivia-Chen-Xu <oliviachenxu@gmail.com>
2021-10-16 07:11:02 -0400

    some changes

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@ import 'augmented_images.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
